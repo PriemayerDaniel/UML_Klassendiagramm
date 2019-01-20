3c1ed2540e68a352241d7f21abc84244dfa7c2ac
PriemayerDaniel <45943856+PriemayerDaniel@users.noreply.github.com>
2019-01-20 15:16:24 +0100

    Add files via upload

--- Belegung.java
+++ Belegung.java
@@ -1,144 +1,28 @@
-import java.time.DayOfWeek;
-import java.time.LocalTime;
-import java.time.format.TextStyle;
-import java.util.Locale;
-
-//Belegung enthält 1 Fach und 1 Raum
-//Raum und Fach enthalten Listen auf ihre Belegungen
-//Keine unidirectionale verbindung sondern bidirectional, da ansonst kein Lehrer.exportStundenplan() möglich.
-
-//Lehrer hinzugefügt für eindeutige Zuweisung
 
 public class Belegung {
-    private int unterrichtsEinheit;
-    private Raum raum;
-    private Fach fach;
-    private DayOfWeek unterrichtsTag;
-    private Lehrer lehrer;
-
-    public Belegung(int unterrichtsEinh, Raum rau, Fach fa, DayOfWeek untTag, Lehrer lehr){
-        unterrichtsEinheit = unterrichtsEinh;
-        raum = rau;
-        fach = fa;
-        unterrichtsTag = untTag;
-        lehrer = lehr;
-    }
-
-    public Unterrichtstag getWochentag() {
-        String s = unterrichtsTag.getDisplayName(TextStyle.FULL, new Locale("de")).toUpperCase();
-        return Unterrichtstag.valueOf(s);
-    }
-
-    public Klasse getKlasse() {
-        return raum.getKlasse();
-    }
-
-    //Everything for UnterrichtsEinheit
-
-    public void setUnterrichtsEinheit() {
-        LocalTime time = LocalTime.now();
-        if (time.isAfter(LocalTime.parse("7:50")) && time.isBefore(LocalTime.parse("8:40"))) {
-            unterrichtsEinheit = 1;
-        } else if (time.isAfter(LocalTime.parse("8:40")) && time.isBefore(LocalTime.parse("9:30"))) {
-            unterrichtsEinheit = 2;
-        } else if (time.isAfter(LocalTime.parse("9:40")) && time.isBefore(LocalTime.parse("10:30"))) {
-            unterrichtsEinheit = 3;
-        } else if (time.isAfter(LocalTime.parse("10:30")) && time.isBefore(LocalTime.parse("11:20"))) {
-            unterrichtsEinheit = 4;
-        } else if (time.isAfter(LocalTime.parse("11:20")) && time.isBefore(LocalTime.parse("12:10"))) {
-            unterrichtsEinheit = 5;
-        } else if (time.isAfter(LocalTime.parse("12:20")) && time.isBefore(LocalTime.parse("13:10"))) {
-            unterrichtsEinheit = 6;
-        } else if (time.isAfter(LocalTime.parse("13:10")) && time.isBefore(LocalTime.parse("14:00"))) {
-            unterrichtsEinheit = 7;
-        } else if (time.isAfter(LocalTime.parse("14:00")) && time.isBefore(LocalTime.parse("14:50"))) {
-            unterrichtsEinheit = 8;
-        } else if (time.isAfter(LocalTime.parse("15:00")) && time.isBefore(LocalTime.parse("15:50"))) {
-            unterrichtsEinheit = 9;
-        } else if (time.isAfter(LocalTime.parse("15:50")) && time.isBefore(LocalTime.parse("16:40"))) {
-            unterrichtsEinheit = 10;
-        }
-
-
-    }
-
-    public void setUnterrichtsEinheit(LocalTime time) {
-
-        if (time.isAfter(LocalTime.parse("7:50")) && time.isBefore(LocalTime.parse("8:40"))) {
-            unterrichtsEinheit = 1;
-        } else if (time.isAfter(LocalTime.parse("8:40")) && time.isBefore(LocalTime.parse("9:30"))) {
-            unterrichtsEinheit = 2;
-        } else if (time.isAfter(LocalTime.parse("9:40")) && time.isBefore(LocalTime.parse("10:30"))) {
-            unterrichtsEinheit = 3;
-        } else if (time.isAfter(LocalTime.parse("10:30")) && time.isBefore(LocalTime.parse("11:20"))) {
-            unterrichtsEinheit = 4;
-        } else if (time.isAfter(LocalTime.parse("11:20")) && time.isBefore(LocalTime.parse("12:10"))) {
-            unterrichtsEinheit = 5;
-        } else if (time.isAfter(LocalTime.parse("12:20")) && time.isBefore(LocalTime.parse("13:10"))) {
-            unterrichtsEinheit = 6;
-        } else if (time.isAfter(LocalTime.parse("13:10")) && time.isBefore(LocalTime.parse("14:00"))) {
-            unterrichtsEinheit = 7;
-        } else if (time.isAfter(LocalTime.parse("14:00")) && time.isBefore(LocalTime.parse("14:50"))) {
-            unterrichtsEinheit = 8;
-        } else if (time.isAfter(LocalTime.parse("15:00")) && time.isBefore(LocalTime.parse("15:50"))) {
-            unterrichtsEinheit = 9;
-        } else if (time.isAfter(LocalTime.parse("15:50")) && time.isBefore(LocalTime.parse("16:40"))) {
-            unterrichtsEinheit = 10;
-        }
-
-
-    }
-
-    public void setUnterrichtsEinheit(int i) {
-        unterrichtsEinheit = i;
-    }
-
-    public int getUnterrichtsEinheit(){
-        return unterrichtsEinheit;
-    }
-
-
-    //Everything for Fach
-    public void addFach(Fach newFach) {
-        fach = newFach;
-    }
-
-    public Fach getFach() {
-        return fach;
-    }
-
-
-    //Everything for unterrichtsTag
-    public void setUnterrichtsTag(DayOfWeek x) {
-        unterrichtsTag = x;
-    }
-
-    public DayOfWeek getUnterrichtsTag() {
-        return unterrichtsTag;
-    }
-
-    //Everything for Raum
-    public void setRaum(Raum x) {
-        raum = x;
-    }
-
-    public Raum getRaum() {
-        return raum;
-    }
-
-    //Everything for Lehrer
-
-    public void setLehrer(Lehrer lehrer) {
-        this.lehrer = lehrer;
-    }
-
-    public Lehrer getLehrer(){
-        return lehrer;
-    }
+	
+	//variablen
+	private int unterrichtseinheit;
+	//??????
+	private Lehrer lehrer;
+	private Klasse klasse;
+	private Unterrichtstagenum unterrichtstag;
+	
+	//methoden
+	public Klasse getKlasse() {
+		return klasse;
+	}
+	
+	public Lehrer getLehrer() {
+		return lehrer;
+	}
+	
+	public Unterrichtstag getWochentag() {
+		return unterrichtstag; 
+	}
+	
 }
-
-//Sinvoller mit DayOfWeek, da diese bereits sinvolle funtionen enthält.
-enum Unterrichtstag {
-    MONTAG, DIENSTAG, MITTWOCH, DONNERSTAG, FREITAG, SAMSTAG, SONNTAG
-}
-
+unterrichtsEinheit: int
++getWochentag(): Unterrichtstag
++getKlasse(): Klasse
++getLehrer(): Lehre
--- Raum.java
+++ Raum.java
@@ -1,91 +1,35 @@
-import java.time.DayOfWeek;
-import java.time.format.TextStyle;
-import java.util.Hashtable;
-import java.util.Locale;
+import java.util.ArrayList;
 
 public class Raum {
-
-    //Variables
-    private String raumNummer;
-    private int maxSitzplaetze;
+    
+	
+	//variablen
+	private String raumnummer;
+    private int maxsitzplaetze;
     private Raumtyp raumtyp;
-    private Hashtable<String, Belegung> belegung = new Hashtable<String, Belegung>();                   //Key: "Wochenkürzel englisch"  + Stundennummer      z.B.: 3Stunde am Dienstang TUE3
-    private Klasse klasse;
+    public Klasse Stammklasse;
+    //private Hashtable<Sting, Belegung> blegung = new Hashtable<String, Belegung>();
 
-    //Constructors
-    public Raum(String raumNum, int maxSitpl, Raumtyp raumty, Hashtable<String, Belegung> x, Klasse klas) {
-        raumNummer = raumNum;
-        maxSitzplaetze = maxSitpl;
-        raumtyp = raumty;
-        belegung.putAll(x);
-        klasse = klas;
+    //konstruktoren
+    //???
+    
+    
+    
+    //methoden
+    	
+    public int getMaxsitzplaetze() {
+        return maxsitzplaetze;
     }
 
-    public Raum(String raumNum, int maxSitpl, Raumtyp raumty, Klasse klas) {
-        raumNummer = raumNum;
-        maxSitzplaetze = maxSitpl;
-        raumtyp = raumty;
-        klasse = klas;
+    public String getRaumnummer() {
+        return raumnummer;
     }
-
-
-    //Methodes
-    public String getRaumNummer() {
-        return raumNummer;
-    }
-
-    public void exportBelegung() {
-        Belegung jetzt;
-        for (int day = 1; day < 8; day++) {
-            for (int einheit = 1; einheit < 12; einheit++) {
-                jetzt = getBelegung(DayOfWeek.of(day),einheit);
-                System.out.print(DayOfWeek.of(day).getDisplayName(TextStyle.FULL, Locale.GERMAN) + "  ");
-                System.out.print(einheit + " Stunde  ");
-                if(jetzt != null){
-                    System.out.print(jetzt.getLehrer().getKuerzel());
-                    System.out.println(jetzt.getFach().getName());
-                }
-                else{
-                    System.out.println("");
-                }
-                }
-        }
-    }
-
-    public int getMaxSitzplaetze() {
-        return maxSitzplaetze;
-    }
-
+    
     public Raumtyp getRaumtyp() {
-        return raumtyp;
+    	return raumtyp;
     }
-
-    public void setRaumtyp(Raumtyp x) {
-        raumtyp = x;
-    }
-
-    public void addBelegung(Belegung bel) {
-        belegung.put((bel.getUnterrichtsTag().getDisplayName(TextStyle.SHORT, Locale.ENGLISH).toUpperCase() + bel.getUnterrichtsEinheit()), bel);
-    }
-
-    public Belegung getBelegung(DayOfWeek tag, int unterrichtsEinheit) {
-        return belegung.get(tag.getDisplayName(TextStyle.SHORT, Locale.ENGLISH).toUpperCase() + unterrichtsEinheit);
-    }
-
-    public void removeBelegung(DayOfWeek tag, int unterrichtsEinheit) {
-        belegung.remove(tag.getDisplayName(TextStyle.SHORT, Locale.ENGLISH).toUpperCase() + unterrichtsEinheit);
-    }
-
-    public void setKlasse(Klasse klas) {
-        klasse = klas;
-    }
-
-    public Klasse getKlasse() {
-        return klasse;
-    }
+    
+    
+    
+   // ArrayList<Fach> Faecher = new ArrayList<Fach>();
 }
-
-enum Raumtyp {
-    KLASSENZIMMER, LABORRAUM
-}
-
