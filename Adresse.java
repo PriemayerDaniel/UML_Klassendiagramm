5e06c98bb1a2ef32b2c71ce98cb4cc57a74f17cc
PriemayerDaniel <45943856+PriemayerDaniel@users.noreply.github.com>
2019-01-20 15:39:51 +0100

    Add files via upload

--- Adresse.java
+++ Adresse.java
@@ -1,11 +1,24 @@
 
 
 public class Adresse {
+	
+	//variablen
     private String ort;
     private String straße;
     private Integer hausnummer;
     private Integer plz;
 
+    //konstruktoren
+    public Adresse(String ORT, String str, Integer hnummer, Integer PLZ) {
+    	ort = ORT;
+    	straße = str;
+    	hausnummer = hnummer;
+    	plz = PLZ;
+    }
+    
+    
+    
+    //mehtoden
     public Integer getHausnummer() {
         return hausnummer;
     }
